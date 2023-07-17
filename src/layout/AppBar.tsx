198b8b20525dd40b4331afbeca0ff1dd6509057c
Nahuel Hernández <jnahuelhernandez@gmail.com>
2023-07-17 18:51:56 -0400

    feat: icon responsive on auth page

--- src/layout/AppBar.tsx
+++ src/layout/AppBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Toolbar } from '@mui/material';
+import { Box, Container, Toolbar, useMediaQuery } from '@mui/material';
 import MuiAppBar from '@mui/material/AppBar';
 import { useT } from '@transifex/react';
 import { Link } from 'react-router-dom';
@@ -14,19 +14,27 @@ export interface AppBarProps {
   logout?: () => void | undefined;
 }
 
+const styleWithMobileLogo = {
+  width: '125px',
+};
+
 export function AppBar({ auth = true, logout }: AppBarProps) {
   const t = useT();
+  const matches = useMediaQuery('(min-width:600px)');
+
   return (
     <MuiAppBar position="static" className="bg-darkest" elevation={0}>
       <Container maxWidth={false} className="lg:px-20 md:px-12 sm:px-10 px-6 mx-0">
         <Toolbar disableGutters sx={{ height: '100px' }}>
           <Link to="/" style={{ textDecoration: 'none', color: '#fff' }}>
-            <img src={OCPLogo} alt="logo" />
+            <img style={matches && auth ? {} : styleWithMobileLogo} src={OCPLogo} alt="logo" />
           </Link>
 
           <Box sx={{ display: 'flex', flexGrow: 1, justifyContent: 'flex-end' }}>
             <SelectLanguageComponent />
-            {auth && !!logout && <Button className="ml-2" size="small" label={t('Logout')} onClick={() => logout()} />}
+            {auth && !!logout && (
+              <Button noIcon={!matches} className="ml-2" size="small" label={t('Logout')} onClick={() => logout()} />
+            )}
             {!auth && <Button className="ml-2" size="small" label={t('Login')} component={Link} to="/login" />}
           </Box>
         </Toolbar>
