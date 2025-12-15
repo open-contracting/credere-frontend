d60432981f4fdbcd058a329877ab107d41e753f2
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-12-15 16:04:32 -0500

    build(tailwind): Run npx @tailwindcss/upgrade, started in ff4fbb9. Keep preflight disabled.
    "In v3 there was a corePlugins option you could use to completely disable certain utilities in the framework. This is no longer supported in v4."
    https://tailwindcss.com/docs/upgrade-guide#disabling-core-plugins
    https://v3.tailwindcss.com/docs/preflight#disabling-preflight
    https://tailwindcss.com/docs/preflight#disabling-preflight
    
    "Additionally, in v4 imports and vendor prefixing is now handled for you automatically, so you can remove postcss-import and autoprefixer if they are in your project"
    https://tailwindcss.com/docs/upgrade-guide#using-postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,9 +28,5 @@ export default {
     },
     colors: COLORS,
   },
-  corePlugins: {
-    // Remove the Tailwind CSS preflight styles so it can use Material UI's preflight instead (CssBaseline).
-    preflight: false,
-  },
   plugins: [],
 };
