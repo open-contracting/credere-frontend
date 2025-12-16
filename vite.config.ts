7a97fcc9fc1ada0457b8e83353ce6ff56d45f134
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-12-15 23:48:24 -0500

    build(tailwind): Use Vite plugin instead of PostCSS plugin https://tailwindcss.com/docs/upgrade-guide#using-vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, loadEnv } from "vite";
 import checker from "vite-plugin-checker";
@@ -16,6 +17,7 @@ export default ({ mode }) => {
     plugins: [
       react(),
       tsconfigPaths(),
+      tailwindcss(),
       checker({
         typescript: true,
       }),
