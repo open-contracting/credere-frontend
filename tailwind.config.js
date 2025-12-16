f355af097243f5268dd68c46aa7fb4374d80b0fa
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-12-16 00:39:50 -0500

    build(tailwind): Move content configuration to @source directives

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", ".storybook/preview.tsx"],
   important: true, // Tailwind layers would be lower priority than MUI classes.
 };
